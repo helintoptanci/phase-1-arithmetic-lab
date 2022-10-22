44808dfef708274b89e8408d3447c2387b669145
Helin <helintoptanci3@outlook.com>
2022-10-22 23:10:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 
-// Write your code here
+let num1 = 62;
+let num2 = 1;
+let multiply = num1 * num2;
+console.log(multiply);
+
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random)
+
+let num3 = 19;
+let num4 = 5;
+let mod= num3 % num4;
+console.log(mod);
+
+
+let max = Math.max(1, 2, 20, 4, 5);
+console.log(max);
+
+
+
+
+
+
+
+ 
+
+ 
